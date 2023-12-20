e1c2af6c72b1d9a389141ef3c67aafb09f0b7a6c
Brian Mason <brian.mason@slim.ai>
2023-12-20 07:48:49 -0500

    added readme

--- workspaces/python/generate.sh
+++ workspaces/python/generate.sh
@@ -1,5 +1,5 @@
 rm -r -f workspace_client
 openapi-generator-cli generate -i ../openapi.yml -g python -o .  -c config.yaml  
-rm mpenapitools.json		
+rm openapitools.json	
 rm workspace_client_README.md
 
